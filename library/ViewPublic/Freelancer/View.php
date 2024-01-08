1893b130ec8b1de02715a970b9e415da11eb01c3
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2024-01-08 10:33:36 +0200

    Create View.php

---
+++ library/ViewPublic/Freelancer/View.php
@@ -0,0 +1,9 @@
+<?php
+
+class Freelancers_ViewPublic_Freelancer_View extends XenForo_ViewPublic_Base
+{
+    public function renderHtml()
+    {
+        // Define HTML rendering logic here
+    }
+}
